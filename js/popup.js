95622785aea76d6ab0caf624b60f519300ccfde8
Vladimir Novikov <NovikovVV-Tver@yandex.ru>
2017-05-10 20:44:39 +0300

    Оформление главной страницы и страницы каталога
    Оформление главной страницы и страницы каталога

---
+++ js/popup.js
@@ -0,0 +1,31 @@
+var feedbackLink = document.querySelector(".contacts .btn");
+var feedbackForm = document.querySelector(".feedback");
+var overlay = document.querySelector(".overlay");
+var close = document.querySelector(".modal-content-close");
+
+feedbackLink.addEventListener("click", function(event) {
+				event.preventDefault();
+				feedbackForm.classList.add("modal-content-show");
+				overlay.classList.add("overlay-show");
+			});
+			
+
+close.addEventListener("click", function(event) {
+				event.preventDefault();
+				feedbackForm.classList.remove("modal-content-show");
+				overlay.classList.remove("overlay-show");
+			});
+			
+window.addEventListener("keydown", function(event) {
+				if (event.keyCode === 27) {
+					if (feedbackForm.classList.contains("modal-content-show")) {
+						feedbackForm.classList.remove("modal-content-show");
+						overlay.classList.remove("overlay-show");
+					}
+				}
+			});
+			
+overlay.addEventListener("click", function(event){
+				feedbackForm.classList.remove("modal-content-show");
+				overlay.classList.remove("overlay-show");
+			});
