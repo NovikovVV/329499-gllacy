77878e8bf244d7929e520844d8a1d6fca58529a5
Vladimir Novikov <NovikovVV-Tver@yandex.ru>
2017-05-14 18:35:07 +0300

    Некоторые правки
    Убрал стилизацию по тегам, убрал тройные селектроы

--- js/popup.js
+++ js/popup.js
@@ -4,28 +4,28 @@ var overlay = document.querySelector(".overlay");
 var close = document.querySelector(".modal-content-close");
 
 feedbackLink.addEventListener("click", function(event) {
-				event.preventDefault();
-				feedbackForm.classList.add("modal-content-show");
-				overlay.classList.add("overlay-show");
-			});
+	event.preventDefault();
+	feedbackForm.classList.add("modal-content-show");
+	overlay.classList.add("overlay-show");
+});
 			
 
 close.addEventListener("click", function(event) {
-				event.preventDefault();
-				feedbackForm.classList.remove("modal-content-show");
-				overlay.classList.remove("overlay-show");
-			});
+	event.preventDefault();
+	feedbackForm.classList.remove("modal-content-show");
+	overlay.classList.remove("overlay-show");
+});
 			
 window.addEventListener("keydown", function(event) {
-				if (event.keyCode === 27) {
-					if (feedbackForm.classList.contains("modal-content-show")) {
-						feedbackForm.classList.remove("modal-content-show");
-						overlay.classList.remove("overlay-show");
-					}
-				}
-			});
+	if (event.keyCode === 27) {
+		if (feedbackForm.classList.contains("modal-content-show")) {
+			feedbackForm.classList.remove("modal-content-show");
+			overlay.classList.remove("overlay-show");
+		}
+	}
+});
 			
 overlay.addEventListener("click", function(event){
-				feedbackForm.classList.remove("modal-content-show");
-				overlay.classList.remove("overlay-show");
-			});
+	feedbackForm.classList.remove("modal-content-show");
+	overlay.classList.remove("overlay-show");
+});
